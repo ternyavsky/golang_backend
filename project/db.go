90ae378f36c243794c756678c0a391b94638197f
ternyavsky <ilya.ternyavsky@yandex.ru>
2023-07-29 21:19:13 +0300

    db go

---
+++ project/db.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+  _ "github.com/mattn/go-sqlite3"
+)
+
+type Task struct {
+	ID    int    `json:"id"`
+	TITLE string `json:"title"`
+	DESC  string `json:"description"`
+	DATE  string `json:"date"`
+}
+
+func getTasks() []Task{
+	db, err := sql.Open("sqlite3", "todo.db")
+  if err != nil{
+    log.Fatal(err)
+  }
+	defer db.Close()
+	rows, err := db.Query("select * from tasks")
+  if err != nil{
+    log.Fatal(err)
+  }
+	defer db.Close()
+
+	tasks := []Task{}
+	for rows.Next() {
+		t := Task{}
+		rows.Scan(&t.ID, &t.TITLE, &t.DESC, &t.DATE)
+		tasks = append(tasks, t)
+	}
+  return tasks
+}
+
+func createTask(title string, description string) Task{
+	db, _ := sql.Open("sqlite3", "todo.db")
+
+	defer db.Close()
+  res, err := db.Exec("insert into tasks(title, description, date)values($1, $2 ,datetime('now'))", title, description)
+  if err != nil{
+    log.Fatal(err)
+  }
+ defer db.Close() 
+  id, _ := res.LastInsertId()
+  row, err := db.Query("select * from tasks where id=$1", id)
+  t := Task{}
+  row.Scan(&t.ID, &t.TITLE, &t.DESC, &t.DATE)
+  return t
+
+
+}
---
+++ project/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	ID   string `json:"id"`
+	NAME string `json:"username"`
+	AGE  uint8  `json:"age"`
+}
+type Cart struct {
+	ID    string  `json:"id"`
+	TITLE string  `json:"title"`
+	PRICE float32 `json:"price"`
+	USER  User    `json:"user"`
+}
+
+var users = []User{
+	{ID: "1", NAME: "Valentin", AGE: 16},
+	{ID: "2", NAME: "Arkado$", AGE: 16},
+	{ID: "3", NAME: "Ilya", AGE: 17},
+}
+
+var items = []Cart{
+	{ID: "1", TITLE: "Watch", PRICE: 32.90, USER: users[0]},
+	{ID: "2", TITLE: "Glasses", PRICE: 12.20, USER: users[1]},
+	{ID: "3", TITLE: "T-Shirt", PRICE: 52.30, USER: users[2]},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/users", getUsers)
+	router.GET("/about", funcAbout)
+	router.GET("/", funcHome)
+	router.GET("/users/:id", getUser)
+	router.POST("/add", pushUser)
+  router.POST("/createtask", createT)
+  router.GET("/task", getT)
+	router.GET("/items/:id", getDetailItems)
+	router.GET("/items/", getItems)
+
+	router.Run("localhost:8000")
+
+}
+func createT(c *gin.Context){
+  desc := c.PostForm("description")
+  title := c.PostForm("title")
+
+  a := createTask(title, desc)
+  c.IndentedJSON(http.StatusOK, a)
+}
+func getT(c *gin.Context){
+  a := getTasks()
+  c.IndentedJSON(http.StatusOK, a)
+}
+func getItems(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, items)
+}
+
+func getDetailItems(c *gin.Context) {
+	itemId := c.Param("id")
+	for _, val := range items {
+		if val.ID == itemId {
+			c.IndentedJSON(http.StatusOK, val)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Item not found("})
+}
+func funcAbout(c *gin.Context) {
+	c.HTML(
+		http.StatusOK,
+		"about.html",
+		gin.H{
+			"title": "About",
+		},
+	)
+}
+
+func funcHome(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"/": "home"})
+}
+
+func getUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
+
+func getUser(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range users {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
+}
+
+func pushUser(c *gin.Context) {
+	var newUser User
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+	users = append(users, newUser)
+	c.IndentedJSON(http.StatusCreated, newUser)
+}
